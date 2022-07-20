0071e3c43a19ca2ddee9644ba327d0dbe547e52e
mahan <mahan.timalsena@danphecare.com>
2022-07-20 23:49:58 +0545

    missing Controller

---
+++ store-appV2-BACKEND/Controllers/UsersController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using store_appV2_BACKEND.Data;
+using store_appV2_BACKEND.Models;
+using System.Collections;
+
+namespace store_appV2_BACKEND.Controllers
+{
+    [EnableCors("_myAllowSpecificOrigins")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly ApplicationDBContext _context;
+
+        public UsersController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable>> GetUsers()
+        {
+            if (_context.Users == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.Users.Where(x => x.IsActive).ToListAsync();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> PostInventory(User user1) 
+        {
+            if (_context.Users == null)
+            {
+                return Problem("Entity set 'ApplicationDBContext.Users'  is null.");
+            }
+            _context.Users.Add(user1);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (UsersExists(user1.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Ok(new { status = 200, id = user1.Id });
+        }
+
+        private bool UsersExists(int id)
+        {
+            return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
